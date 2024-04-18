9a885bc757a9c2ce7fa8cf0493d770f24e4d6ee7
JuanPablo <96244210+jbidabehere@users.noreply.github.com>
2024-04-17 21:35:48 -0300

    Clase 1.1

--- 00.Clase/HolaMundo/obj/Debug/net8.0/HolaMundo.AssemblyInfo.cs
+++ 00.Clase/HolaMundo/obj/Debug/net8.0/HolaMundo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HolaMundo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69ce1d2943d5e4b93b5e724321aace93e077e17e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HolaMundo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HolaMundo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
