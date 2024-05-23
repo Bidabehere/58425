a56c06aa344f7cca5e130c4e287e822e19001d53
JuanPablo <96244210+jbidabehere@users.noreply.github.com>
2024-05-22 22:34:49 -0300

    Clase 10

---
+++ 10. Clase/ConsoleApp1/Database/GestorBaseDeDatos.cs
@@ -0,0 +1,137 @@
+﻿using ConsoleApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Database
+{
+    internal class GestorBaseDeDatos
+    {
+        private string connectionString;
+
+        public GestorBaseDeDatos()
+        {
+            connectionString = @"Server=localhost\SQLEXPRESS;Database=SistemaGestion;Trusted_Connection=True;";
+
+        }
+
+        public bool DeleteUser(int userId)
+        {
+            using(SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Usuario WHERE Id = @id";
+                SqlCommand command = new SqlCommand(query, conn);
+                conn.Open();
+                command.Parameters.AddWithValue("id", userId);
+
+                return command.ExecuteNonQuery() > 0;
+            }
+            
+        }
+
+        public bool CreateUser(Usuario usuario)
+        {
+            using(SqlConnection connection = new SqlConnection(this.connectionString))
+            {
+                string query = "INSERT INTO Usuario(nombre, apellido,nombreUsuario,contraseña,mail) values(@nombre, @apellido,@nombreUsuario,@password,@email)";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("nombre", usuario.Nombre);
+                command.Parameters.AddWithValue("apellido", usuario.Apellido);
+                command.Parameters.AddWithValue("nombreUsuario", usuario.NombreUsuario);
+                command.Parameters.AddWithValue("password", usuario.Password);
+                command.Parameters.AddWithValue("email", usuario.Email);
+
+                connection.Open();
+
+                return command.ExecuteNonQuery() > 0;
+
+            }
+        } 
+        public Usuario GetUserById(int id)
+        {
+            using(SqlConnection connection = new SqlConnection(this.connectionString))
+            {
+                string query = "SELECT * FROM Usuario WHERE id = @id";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("id", id);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader(); 
+
+                if (reader.Read())
+                {
+                    int userId = Convert.ToInt32(reader[0]);
+                    string nombre = reader.GetString(1);
+                    string apellido = reader.GetString(2);
+                    string nombreUsuario = reader.GetString(3);
+                    string password = reader.GetString(4); 
+                    string email = reader.GetString(5);
+
+                    Usuario usuario = new Usuario(userId, nombre,apellido,nombreUsuario,password,email);
+                    
+                    return usuario;
+                }
+                throw new Exception("Id no fue encontrado");
+            }
+            
+
+        }
+
+
+        public bool UpdateUser(int id, Usuario usuario)
+        {
+            using(SqlConnection connection = new SqlConnection(this.connectionString))
+            {
+                string query = "UPDATE Usuario SET nombre = @nombre, apellido = @apellido, nombreUsuario= @nombreUsuario, contraseña = @password, mail = @email WHERE Id = @id";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("nombre", usuario.Nombre);
+                command.Parameters.AddWithValue("apellido", usuario.Apellido);
+                command.Parameters.AddWithValue("nombreUsuario", usuario.NombreUsuario);
+                command.Parameters.AddWithValue("password", usuario.Password);
+                command.Parameters.AddWithValue("email", usuario.Email);
+                //Id que esta en el WHERE
+                command.Parameters.AddWithValue("id", id);
+
+                connection.Open();
+                return command.ExecuteNonQuery() > 0;
+            }
+        }
+
+        public List<Usuario> ListaUsuarios()
+        {
+            List<Usuario> listaUsuarios = new List<Usuario>();
+            string query = "SELECT * FROM Usuario";
+            using(SqlConnection connection = new SqlConnection(this.connectionString))
+            {
+                connection.Open();
+                using(SqlCommand command = new SqlCommand(query,connection))
+                {
+                    using(SqlDataReader dataReader = command.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            Usuario usuario = new Usuario();
+                            usuario.Id = Convert.ToInt32(dataReader["Id"]);
+                            usuario.Nombre = dataReader["Nombre"].ToString();
+                            usuario.Apellido = dataReader["Apellido"].ToString();
+                            usuario.NombreUsuario = dataReader["NombreUsuario"].ToString();
+                            usuario.Password = dataReader["Contraseña"].ToString();
+                            usuario.Email = dataReader["Mail"].ToString();
+
+                            listaUsuarios.Add(usuario);
+
+                        }
+                    }
+                }
+            }
+            return listaUsuarios;
+
+        }
+    }
+}
---
+++ 10. Clase/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using ConsoleApp1.Database;
+using System.Data.SqlClient;
+
+
+namespace ConsoleApp1.Models
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GestorBaseDeDatos gestorBaseDeDatos = new GestorBaseDeDatos();
+
+            try
+            {
+
+               // if (gestorBaseDeDatos.DeleteUser(7))
+               // {
+               //     Console.WriteLine("Usuario eliminado");
+               // }
+
+                Usuario newUser = new Usuario("Carlos", "Perez", "peres32","123456789","carlo@mail.com");
+                //if(gestorBaseDeDatos.CreateUser(newUser))
+                //{
+                //    Console.WriteLine("Usuario creado!");
+                //}
+                Usuario editUser = new Usuario("Carlos", "Perez", "peres32", "123456789", "carlo@mail.com");
+
+                if (gestorBaseDeDatos.UpdateUser(6, editUser))
+                {
+                    Console.WriteLine("Usuario actualizado!");
+                }
+
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
